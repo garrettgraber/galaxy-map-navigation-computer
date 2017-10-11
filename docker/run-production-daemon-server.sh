9a01c113e36e025a91deb0c1816ef40bb3c6c97c
garrettgraber <garrett_graber@yahoo.com>
2017-10-10 21:15:00 -0600

    new production daemon server script

---
+++ docker/run-production-daemon-server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+./build.sh
+# echo "Deleting "
+docker rm -f navi-computer-node 
+echo "Running navi-computer-node.."
+
+docker run --name navi-computer-node -d --env NODE_ENV=production  --link hyperspace-data:graph -v /${PWD}/../://root/app  -p 80:8117  -e "RUN_STATUS=server" navi-computer-node
+
