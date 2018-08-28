deeab1c44da8ecaa175f3b6979278674a85244a0
blade-runner <garrett_graber@yahoo.com>
2018-08-27 19:11:17 -0600

    Removed link to hyperspace data container for production load script.

--- docker/load-production-graph-data.sh
+++ docker/load-production-graph-data.sh
@@ -6,5 +6,5 @@ docker rm -f navi-computer-node
 echo "Running navi-computer-node.."
 
 
-docker run --name navi-computer-node  --env NODE_ENV=production --link hyperspace-data:graph  -v /${PWD}/../://root/app  -p 80:8117  -e "RUN_STATUS=loader" navi-computer-node
+docker run --name navi-computer-node  --env NODE_ENV=production  -v /${PWD}/../://root/app  -p 80:8117  -e "RUN_STATUS=loader" navi-computer-node
 
